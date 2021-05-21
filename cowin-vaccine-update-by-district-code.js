9c1d5f2d7580f777e0decbf2f34a671f208c8dd7
Sanskar Agarwal <53567863+Sanskar1123@users.noreply.github.com>
2021-05-21 18:00:40 +0530

    Update cowin-vaccine-update-by-district-code.js
    Alerts can be now filtered for dose 1 and dose 2 slots

--- cowin-vaccine-update-by-district-code.js
+++ cowin-vaccine-update-by-district-code.js
@@ -1,6 +1,7 @@
 var dist_code = "141";                  //district code of the district (Central Delhi in this case)
-var startDate = new Date(2021,05,16);   //start date in yyyy,mm,dd format
-var endDate = new Date(2021,05,16);     //end date in yyyy,mm,dd format
+var startDate = new Date(2021,05,23);   //start date in yyyy,mm,dd format
+var endDate = new Date(2021,05,21);     //end date in yyyy,mm,dd format
+var dose = 1;                           //enter 1 to set alert for dose 1 slots, 2 for dose 2 slots and 0 for any of dose 1 or dose 2 slots
 var waitTime = 7;                       //reload time in seconds
 
 Date.prototype.addDay = function(days) {
@@ -36,34 +37,48 @@ function playSound() {
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min); 
   }
-function isBelow18SessionAvailable(sessions) {
+function isAbove18SessionAvailable(sessions) {
     for (var i=0; i < sessions.length; i++) {
-      var session = sessions[i];
-      if (session.min_age_limit == 18 && session.available_capacity != 0) {
-        return true;
-      }
+        var session = sessions[i];
+        if(dose == 0)
+            if (session.min_age_limit == 18 && session.available_capacity > 0) 
+                return true;
+        if(dose == 1)
+            if (session.min_age_limit == 18 && session.available_capacity_dose1 > 0) 
+                return true;
+        if(dose == 2)
+            if (session.min_age_limit == 18 && session.available_capacity_dose2 > 0)
+                return true;
     }
   }
 
 function parser(a) {
-    var capacity_45 = 0;
-    var capacity_18 = 0;
+    var capacity_45_dose1 = 0;
+    var capacity_45_dose2 = 0;
+    var capacity_18_dose1 = 0;
+    var capacity_18_dose2 = 0;
     for (c in a.centers) {
-        if (isBelow18SessionAvailable(a.centers[c].sessions)) {
+        if (isAbove18SessionAvailable(a.centers[c].sessions)) {
             playSound();
           }
+        
         for (s in a.centers[c].sessions) {
-            if (a.centers[c].sessions[s].min_age_limit == 18 && a.centers[c].sessions[s].available_capacity > 0)
-                capacity_18 = capacity_18 + a.centers[c].sessions[s].available_capacity;
-            if (a.centers[c].sessions[s].min_age_limit == 45 && a.centers[c].sessions[s].available_capacity > 0)
-                capacity_45 = capacity_45 + a.centers[c].sessions[s].available_capacity;
+            if (a.centers[c].sessions[s].min_age_limit == 18 && a.centers[c].sessions[s].available_capacity_dose1 > 0)
+                capacity_18_dose1 = capacity_18_dose1 + a.centers[c].sessions[s].available_capacity_dose1;
+            if (a.centers[c].sessions[s].min_age_limit == 18 && a.centers[c].sessions[s].available_capacity_dose2 > 0)
+                capacity_18_dose2 = capacity_18_dose2 + a.centers[c].sessions[s].available_capacity_dose2;
+            if (a.centers[c].sessions[s].min_age_limit == 45 && a.centers[c].sessions[s].available_capacity_dose1 > 0)
+                capacity_45_dose1 = capacity_45_dose1 + a.centers[c].sessions[s].available_capacity_dose1;
+            if (a.centers[c].sessions[s].min_age_limit == 45 && a.centers[c].sessions[s].available_capacity_dose2 > 0)
+                capacity_45_dose2 = capacity_45_dose2 + a.centers[c].sessions[s].available_capacity_dose2;
             }
             
         }        
-        console.log("Total slots availiable:- For 18+ :"+capacity_18+",For 45+ :"+capacity_45);
-        console.log("Total centers available:-"+a.centers.length);
+        console.log("Total dose 1 slots availiable:- For 18+ :"+capacity_18_dose1+",For 45+ :"+capacity_45_dose1);
+        console.log("Total dose 2 slots availiable:- For 18+ :"+capacity_18_dose2+",For 45+ :"+capacity_45_dose2);
+        console.log("Total centers available: "+a.centers.length);
         console.log("");
 }
 
@@ -71,7 +86,7 @@ function seek(){
     console.log("Seek:"+x);
     x= x+1;
     for(i = 0; i<dateArr.length;i++){
-        var URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id="+dist_code+"&date="+dateArr[i]+"ran"+getRandomInt(0,1000000);
+        var URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id="+dist_code+"&date="+dateArr[i]+"&ran"+getRandomInt(0,1000000);
         fetch(URL)
         .then(data=>{return data.json()})
         .then(res=>{parser(res)})
